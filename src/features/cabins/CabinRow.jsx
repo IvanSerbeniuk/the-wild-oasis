a540d7fb4f6bb778d765e0c4d6df63a35894acae
Ivan <serbeniuk.ivan@gmail.com>
2026-01-08 18:33:51 +0200

    reusable context menu

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,7 +7,8 @@ import { HiSquare2Stack } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
-import Table  from "../../ui/Table.jsx";
+import Table from "../../ui/Table.jsx";
+import Menus from "../../ui/Menus.jsx";
 
 // const TableRow = styled.div`
 // 	display: grid;
@@ -87,35 +88,46 @@ function CabinRow({ cabin }) {
 			)}
 
 			<div>
-				<button disabled={isCreating} onClick={() => handleDuplicate()}>
-					<HiSquare2Stack />
-				</button>
 				<Modal>
-					<Modal.Open opens="edit">
-						<button>
-							<HiPencil></HiPencil>
-						</button>
-					</Modal.Open>
-					<Modal.Window name="edit">
-						<CreateCabinForm cabinToEdit={cabin} />
-					</Modal.Window>
+					<Menus.Menu>
+						<Menus.Toggle id={cabinId} />
+						<Menus.List id={cabinId}>
+							<Menus.Button
+								icon={<HiSquare2Stack />}
+								onClick={() => handleDuplicate()}
+							>
+								Duplicate
+							</Menus.Button>
 
-					<Modal.Open opens="delete">
-						<button disabled={isDeleting}>
-							<HiTrash />
-						</button>
-					</Modal.Open>
-					<Modal.Window name="delete">
-						<ConfirmDelete
-							resourceName="cabins"
-							disabled={isDeleting}
-							onConfirm={() => deleteCabin(cabinId)}
-						/>
-					</Modal.Window>
+							<Modal.Open opens="edit">
+								<Menus.Button icon={<HiPencil />}>
+									Edit
+								</Menus.Button>
+							</Modal.Open>
+
+							<Modal.Open opens="delete">
+								<Menus.Button icon={<HiTrash />}>
+									Delete
+								</Menus.Button>
+							</Modal.Open>
+						</Menus.List>
+
+						<Modal.Window name="edit">
+							<CreateCabinForm cabinToEdit={cabin} />
+						</Modal.Window>
+
+						<Modal.Window name="delete">
+							<ConfirmDelete
+								resourceName="cabins"
+								disabled={isDeleting}
+								onConfirm={() => deleteCabin(cabinId)}
+							/>
+						</Modal.Window>
+					</Menus.Menu>
 				</Modal>
 			</div>
 		</Table.Row>
 	);
 }
-  
+
 export default CabinRow;
