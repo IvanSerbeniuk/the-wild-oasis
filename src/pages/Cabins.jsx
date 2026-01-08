8da3155b80e3932a4d6e6d5383bc7bdcb830b18b
Ivan <serbeniuk.ivan@gmail.com>
2026-01-08 09:22:45 +0200

    Cabin Deletions | Modal

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,24 +1,19 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
-import Button from "../ui/Button.jsx";
-import { useState } from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
+import AddCabin from "../features/cabins/AddCabin.jsx";
 
 function Cabins() {
-	const [showForm, setShowForm] = useState(false);
 	return (
 		<>
 			<Row type="horizontal">
 				<Heading as="h1">All cabins</Heading>
 				<p>Filter / Sort</p>
 			</Row>
+
 			<Row type={"vertical"}>
 				<CabinTable />
-				<Button onClick={() => setShowForm((show) => !show)}>
-					Add new cabin
-				</Button>
-				{showForm && <CreateCabinForm />}
+				<AddCabin />
 			</Row>
 		</>
 	);
