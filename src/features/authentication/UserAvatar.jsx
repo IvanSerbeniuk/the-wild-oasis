dba42add4fe1e7872ea121d5f3cd44da46a0f451
Ivan <serbeniuk.ivan@gmail.com>
2026-01-13 18:16:20 +0200

    Building The App Header

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUser.js";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -11,7 +12,7 @@ const StyledUserAvatar = styled.div`
 
 const Avatar = styled.img`
   display: block;
-  width: 4rem;
+  //width: 4rem;
   width: 3.6rem;
   aspect-ratio: 1;
   object-fit: cover;
@@ -19,3 +20,22 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+
+function UserAvatar() {
+	const {user} = useUser()
+	const {fullName, avatar} = user.user_metadata
+	// console.log(user);
+	return (
+		<StyledUserAvatar>
+			<Avatar
+				src={avatar || "default-user.jpg"}
+				alt={`Avatar of ${fullName}`}
+			></Avatar>
+
+			<span>{fullName}</span>
+		</StyledUserAvatar>
+	);
+}
+
+export default UserAvatar;
